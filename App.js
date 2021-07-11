5f4c6da5e1805112951058de033c3eb0c11c5113
Guesmi Aslen <aslen9@hotmail.fr>
2021-07-11 20:52:12 +0200

    response lOVE

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>mise a jour aslen </Text>
+      <Text>I love You too </Text>
       <StatusBar style="auto" />
     </View>
   );
