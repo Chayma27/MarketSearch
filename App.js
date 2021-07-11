d514dd17cb42b8de8a7c31eba6a55524643ff4ee
Guesmi Aslen <aslen9@hotmail.fr>
2021-07-11 20:46:51 +0200

    mise a jour aslen

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>27 /12 / 2015</Text>
+      <Text>mise a jour aslen </Text>
       <StatusBar style="auto" />
     </View>
   );
