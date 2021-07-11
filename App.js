e3a9c2d84224303d6985a10713d8bfd80e8a531d
Guesmi Aslen <aslen9@hotmail.fr>
2021-07-11 20:44:45 +0200

    date Love

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>13 aout 2021 branche Aslen</Text>
+      <Text>27 /12 / 2015</Text>
       <StatusBar style="auto" />
     </View>
   );
