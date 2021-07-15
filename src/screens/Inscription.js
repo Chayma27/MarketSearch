ea2b581db8dfc9a05c9d35c86e9ac33cd50f9a7c
Chayma <arfaouichayma27@gmail.com>
2021-07-15 21:38:23 +0200

    screens/connexion finished

---
+++ src/screens/Inscription.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text,View, StyleSheet } from 'react-native';
+
+const Inscription = () => {
+    return(
+        <View style={styles.Container}>
+            <Text> Screen Inscription</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    Container : { 
+        textAlign:"center"
+    }
+})
+
+export default Inscription
