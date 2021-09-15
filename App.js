24a2c0a2fa76b180e009bfee2f80bbb1303d4415
Guesmi Aslen <aslen9@hotmail.fr>
2021-09-15 17:55:14 +0200

    creation de la page home

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Connexion from "./src/screens/Connexion";
 import Inscription from "./src/screens/Inscription";
 import MdpOublie from "./src/screens/MdpOublie";
+import Home from "./src/screens/Home";
 export default function App() {
-  return <MdpOublie />;
+  return <Home />;
 }
