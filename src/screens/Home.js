734551e84a25071a9290b363c279136e7c02b3c0
Chayma <arfaouichayma27@gmail.com>
2021-11-10 17:41:44 +0100

    ajouter des commentaires page home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,13 +3,23 @@ import { View, Text, StyleSheet } from "react-native";
 
 const Home = (props) => {
   return (
-    <View>
-      <Text>Bienvenue Dans l'accueil</Text>
+    <View style= {styles.container}>
+      <Text style = {styles.bienvenue}>Bienvenue Arfaoui Chayma</Text>
+      <Text>Quoi de neuf pour Aujourd'hui !</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex : 1 , 
+    backgroundColor : 'white',
+    marginTop : '5%', 
+    marginLeft : '3%'
+  },
+  bienvenue : { 
+    fontSize : 17, 
+    fontWeight: 'bold'
+  }
 });
 export default Home;
