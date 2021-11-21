2b896623703ca4463eb5a3c00a72f53a028c0d28
Chayma <arfaouichayma27@gmail.com>
2021-11-21 19:07:11 +0100

    ajouter les icons des gatégories

--- src/components/home/Categories.js
+++ src/components/home/Categories.js
@@ -7,17 +7,34 @@ const Categories = props => {
         <View style={styles.contenu}>
 
             
-      <Text style={styles.unboxing}>  
+        <Text style={styles.unboxing}>  
             <FontAwesome5 name="tshirt" size={30}/> {"\n"}Vêtements</Text>
+       
+        <Text style={styles.unboxing}>
+            <FontAwesome5 name="utensils" size={30}/> {"\n"}Cuisine</Text>
+           
+        <Text style={styles.unboxing}>
+            <FontAwesome5 name="tv" size={30}/> {"\n"} Hightech</Text>
+
+        <Text style={styles.unboxing}>
+            <FontAwesome5 name="holly-berry" size={30}/> {"\n"} Décor</Text>
+
+        <Text style={styles.unboxing}>
+            <FontAwesome5 name="star" size={30}/> {"\n"}Beauté</Text>
             
-            <Text style={styles.unboxing} >Cuisine</Text>
-            <Text style={styles.unboxing} >Informatique</Text>
-            <Text style={styles.unboxing} >Décor</Text>
-            <Text style={styles.unboxing} >Beauté</Text>
-            <Text style={styles.unboxing} >SkinCare</Text>
-            <Text style={styles.unboxing} >Electroménager</Text>
-            <Text style={styles.unboxing} >Maquillage</Text>
-            <Text style={styles.unboxing} >Footwear</Text>
+        <Text style={styles.unboxing}>
+        <FontAwesome5 name="playstation" size={30}/> {"\n"}Jouets</Text>
+           
+        <Text style={styles.unboxing}>
+        <FontAwesome5 name="plug" size={30}/> {"\n"}Electroménager</Text>
+            
+        <Text style={styles.unboxing}>
+        <FontAwesome5 name="car-alt" size={30}/> {"\n"}Véhicules</Text>
+        
+
+
+        <Text style={styles.unboxing}>
+            <FontAwesome5 name="leaf" size={30}/> {"\n"}Jardin/Auto/Brico</Text>
         </View>
     )
 }
