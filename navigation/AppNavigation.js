31f04c0e17a64c46f2d09a0ccca03386abf74111
Guesmi Aslen <aslen9@hotmail.fr>
2021-12-06 14:03:37 +0100

    bottomBar Work

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import { Text, View } from 'react-native';
 import Connexion from "../src/screens/Connexion";
@@ -39,11 +40,36 @@ const Tab = createBottomTabNavigator();
 
 function HomeTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} options={{headerShown:false}} />
-      <Tab.Screen name="Categories" component={Categories}  options={{headerShown:false}} />
-      <Tab.Screen name="PublishAnnounce" component={PublishAnnonce} options={{headerShown:false}} />
-      <Tab.Screen name="MyAccount" component={MyAccount} options={{headerShown:false}} />
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === 'Home') {
+          iconName = focused
+            ? 'home-outline'
+            : 'home-outline';
+        } else if (route.name === 'Categories') {
+          iconName = focused ? 'list-outline' : 'list-outline';
+        }
+        else if (route.name === 'PublishAnnounce') {
+          iconName = focused ? 'add-circle-outline' : 'add-circle-outline';
+        }
+        else if (route.name === 'MyAccount') {
+          iconName = focused ? 'person-outline' : 'person-outline';
+        }
+
+        // You can return any component that you like here!
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: 'tomato',
+      tabBarInactiveTintColor: 'gray',
+    })}
+    >
+      <Tab.Screen name="Home" component={Home} options={{title : 'Home' }}  />
+      <Tab.Screen name="PublishAnnounce" component={PublishAnnonce} options={{title : 'Publier Annonce'}} />
+      <Tab.Screen name="Categories" component={Categories}  options={{  title : 'Categories'}} />
+      <Tab.Screen name="MyAccount" component={MyAccount} options={{ title : 'Mon Compte'}} />
     </Tab.Navigator>
   );
 }
@@ -53,7 +79,7 @@ const AppNavigation = () => {
     <NavigationContainer>
    <Stack.Navigator>
       <Stack.Screen name="Connexion" component={Connexion} options={{headerLeft:()=> null}} />
-      <Stack.Screen name="Home" component={HomeTabs}  />
+      <Stack.Screen name="Home" component={HomeTabs} options={{headerLeft:()=> null , headerShown : false}} />
       <Stack.Screen name="Inscription" component={Inscription} />
       <Stack.Screen name="MdpOublie" component={MdpOublie} />
       <Stack.Screen name="NouveauMdp" component={NouveauMdp} />
