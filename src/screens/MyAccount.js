b84a06c56453173027e4e0bb182a68677a48810e
Guesmi Aslen <aslen9@hotmail.fr>
2022-01-28 10:38:24 +0100

    add fleshe icon right for mon compte navigation and style it correctly with touchable opacity click

--- src/screens/MyAccount.js
+++ src/screens/MyAccount.js
@@ -1,7 +1,13 @@
 import React from "react";
-import { StyleSheet, View, ScrollView, Image } from "react-native";
+import { StyleSheet, View, ScrollView, TouchableOpacity } from "react-native";
 import { Text, Card, Button, Icon } from "react-native-elements";
-import { MaterialCommunityIcons, Ionicons, Entypo, FontAwesome } from "@expo/vector-icons";
+import {
+  MaterialCommunityIcons,
+  Ionicons,
+  Entypo,
+  FontAwesome,
+  AntDesign,
+} from "@expo/vector-icons";
 const MyAccount = (props) => {
   return (
     // <View>
@@ -10,39 +16,98 @@ const MyAccount = (props) => {
     <ScrollView>
       <View style={styles.container}>
         <Card>
-          <View style={{ flexDirection: "row" }}>
-            <MaterialCommunityIcons
-              name="shopping-outline"
-              size={24}
-              color="black"
-            />
-            <Text style={styles.cardTitle}> Mes commandes</Text>
-          </View>
+          <TouchableOpacity>
+            <View style={{ flexDirection: "row" }}>
+              <MaterialCommunityIcons
+                name="shopping-outline"
+                size={24}
+                color="black"
+              />
+              <Text style={styles.cardTitle}> Mes commandes</Text>
+              <View
+                style={{
+                  alignItems: "flex-end",
+                  flexGrow: 1,
+                }}
+              >
+                <AntDesign name="rightcircleo" size={24} color="black" />
+              </View>
+            </View>
+          </TouchableOpacity>
           <Card.Divider />
-          <View style={{ flexDirection: "row" }}>
-            <Ionicons name="heart-outline" size={24} color="black" />
-            <Text style={styles.cardTitle}> Mes favoris</Text>
-          </View>
+          <TouchableOpacity>
+            <View style={{ flexDirection: "row" }}>
+              <Ionicons name="heart-outline" size={24} color="black" />
+              <Text style={styles.cardTitle}> Mes favoris</Text>
+              <View
+                style={{
+                  alignItems: "flex-end",
+                  flexGrow: 1,
+                }}
+              >
+                <AntDesign name="rightcircleo" size={24} color="black" />
+              </View>
+            </View>
+          </TouchableOpacity>
           <Card.Divider />
-          <View style={{ flexDirection: "row" }}>
-            <Entypo name="publish" size={23} color="black" />
-            <Text style={styles.cardTitle}> Mes articles publiés</Text>
-          </View>
+          <TouchableOpacity>
+            <View style={{ flexDirection: "row" }}>
+              <Entypo name="publish" size={23} color="black" />
+              <Text style={styles.cardTitle}> Mes articles publiés</Text>
+              <View
+                style={{
+                  alignItems: "flex-end",
+                  flexGrow: 1,
+                }}
+              >
+                <AntDesign name="rightcircleo" size={24} color="black" />
+              </View>
+            </View>
+          </TouchableOpacity>
         </Card>
         <Card>
-          <View style={{ flexDirection: "row" }}>
-          <MaterialCommunityIcons name="card-account-details-outline" size={24} color="black" />
-            <Text style={styles.cardTitle}> Mes informations</Text>
-          </View>
+          <TouchableOpacity>
+            <View style={{ flexDirection: "row" }}>
+              <MaterialCommunityIcons
+                name="card-account-details-outline"
+                size={24}
+                color="black"
+              />
+              <Text style={styles.cardTitle}> Mes informations</Text>
+              <View
+                style={{
+                  alignItems: "flex-end",
+                  flexGrow: 1,
+                }}
+              >
+                <AntDesign name="rightcircleo" size={24} color="black" />
+              </View>
+            </View>
+          </TouchableOpacity>
         </Card>
         <Card>
-          <View style={{ flexDirection: "row" }}>
-          <Ionicons name="information-circle-outline" size={24} color="black" />
-            <Text style={styles.cardTitle}> A propos de nous</Text>
-          </View>
+          <TouchableOpacity>
+            <View style={{ flexDirection: "row" }}>
+              <Ionicons
+                name="information-circle-outline"
+                size={24}
+                color="black"
+              />
+              <Text style={styles.cardTitle}> A propos de nous</Text>
+              <View
+                style={{
+                  alignItems: "flex-end",
+                  flexGrow: 1,
+                }}
+              >
+                <AntDesign name="rightcircleo" size={24} color="black" />
+              </View>
+            </View>
+          </TouchableOpacity>
         </Card>
         <Card>
           <Button
+            onPress={() => props.navigation.navigate("Connexion")}
             icon={
               <FontAwesome
                 name="sign-out"
@@ -70,7 +135,8 @@ const styles = StyleSheet.create({
   cardTitle: {
     fontWeight: "bold",
     color: "#43484D",
-    paddingBottom: "2%",
+    paddingBottom: "3%",
+    marginTop : '1%'
   },
 });
 export default MyAccount;
