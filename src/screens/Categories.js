ab673b4e81c02204d1098de38d1552d2c47138b1
Guesmi Aslen <aslen9@hotmail.fr>
2022-02-18 17:38:12 +0100

    touchale opacity work

--- src/screens/Categories.js
+++ src/screens/Categories.js
@@ -4,105 +4,125 @@ import {
   StyleSheet,
   View,
   ScrollView,
-  Image,
   TouchableOpacity,
-  
+  Image,
 } from "react-native";
-import {Platform} from 'react-native';
+import { Platform } from "react-native";
 
 import { Text, Card, Button, Icon } from "react-native-elements";
 const Categories = (props) => {
   return (
-      <View style={styles.container}>
-
-        <Card containerStyle={styles.card} elevation={1} Button={true}>
-          <Image
-            source={require("../../assets/iconCategories/clothes.png")}
-            style={{  height: "67%", resizeMode: "contain" }}
-          />
-          <Text style={styles.text}>Mode</Text>
-        </Card>
-
-        <Card containerStyle={styles.card} elevation={1}>
-          <Image
-            source={require("../../assets/iconCategories/house-decoration.png")}
-            style={{ height: "67%", resizeMode: "contain" }}
-          />
-          <Text style={styles.text}>Maison & cuisine</Text>
-        </Card>
-        <Card containerStyle={styles.card} elevation={1}>
-          <Image
-            source={require("../../assets/iconCategories/car.png")}
-            style={{  height: "65%", resizeMode: "contain" }}
-          />
-          <Text style={styles.text}>Véhicule</Text>
-        </Card>
-        <Card containerStyle={styles.card} elevation={1}>
-          <Image
-            source={require("../../assets/iconCategories/electronic-device.png")}
-            style={{ height: "65%", resizeMode: "contain" }}
-          />
-          <Text style={styles.text}>Multimédia</Text>
-        </Card>
-        <Card containerStyle={styles.card} elevation={1}>
-          <Image
-            source={require("../../assets/iconCategories/loisir.png")}
-            style={{ height: "70%", resizeMode: "contain" }}
-          />
-          <Text style={styles.text}>Loisir</Text>
-        </Card>
-        <Card containerStyle={styles.card} elevation={1}>
-          <Image
-            source={require("../../assets/iconCategories/sport.png")}
-            style={{  height: "65%", resizeMode: "contain" }}
-          />
-                    <Text style={styles.text}>Sport</Text>
-
-        </Card>
-        <Card containerStyle={styles.card} elevation={1}>
-
-          <Image
-            source={require("../../assets/iconCategories/produits-de-beautee.png")}
-            style={{  height: "55%", resizeMode: "contain" }}
-          />
-          <Text style={styles.text}>Visage et Beauté</Text>
-        </Card>
-        <Card containerStyle={styles.card} elevation={1}>
-          <Image
-            source={require("../../assets/iconCategories/animal-track.png")}
-            style={{ height: "55%", resizeMode: "contain" }}
-          />
-          <Text style={styles.text}>Animaux</Text>
-        </Card>
-      </View>
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.card}
+        onPress={() => console.log("clicked")}
+      >
+        <Image
+          source={require("../../assets/iconCategories/clothes.png")}
+          style={{ width: "100%", height: "80%", resizeMode: "contain" }}
+        />
+        <Text style={styles.text}>Mode</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.card}
+        onPress={() => console.log("clicked")}
+      >
+        <Image
+          source={require("../../assets/iconCategories/house-decoration.png")}
+          style={{ width: "100%", height: "80%", resizeMode: "contain" }}
+        />
+        <Text style={styles.text}>Maison & cuisine</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.card}
+        onPress={() => console.log("clicked")}
+      >
+        <Image
+          source={require("../../assets/iconCategories/car.png")}
+          style={{ width: "100%", height: "80%", resizeMode: "contain" }}
+        />
+        <Text style={styles.text}>Véhicule</Text>
+      </TouchableOpacity>
+           <TouchableOpacity
+        style={styles.card}
+        onPress={() => console.log("clicked")}
+      >
+        <Image
+          source={require("../../assets/iconCategories/electronic-device.png")}
+          style={{ width: "100%", height: "80%", resizeMode: "contain" }}
+        />
+        <Text style={styles.text}>Multimédia</Text>
+      </TouchableOpacity>
+           <TouchableOpacity
+        style={styles.card}
+        onPress={() => console.log("clicked")}
+      >
+        <Image
+          source={require("../../assets/iconCategories/loisir.png")}
+          style={{ width: "100%", height: "80%", resizeMode: "contain" }}
+        />
+        <Text style={styles.text}>Loisir</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.card}
+        onPress={() => console.log("clicked")}
+      >
+        <Image
+          source={require("../../assets/iconCategories/sport.png")}
+          style={{ width: "100%", height: "80%", resizeMode: "contain" }}
+        />
+        <Text style={styles.text}>Sport</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.card}
+        onPress={() => console.log("clicked")}
+      >
+        <Image
+          source={require("../../assets/iconCategories/produits-de-beautee.png")}
+          style={{ width: "100%", height: "80%", resizeMode: "contain" }}
+        />
+        <Text style={styles.text}>Visage et Beauté</Text>
+      </TouchableOpacity>
+      {/* //  */}
+      <TouchableOpacity
+        style={styles.card}
+        onPress={() => console.log("clicked")}
+      >
+        <Image
+          source={require("../../assets/iconCategories/animal-track.png")}
+          style={{ width: "100%", height: "80%", resizeMode: "contain" }}
+        />
+        <Text style={styles.text}>Animaux</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    flexWrap : 'wrap',
-    justifyContent: "space-around",
-    marginTop: "5.5%"
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-around", // space-around
+    marginTop: "5.5%",
+    alignItems: "center",
   },
   card: {
     backgroundColor: "white",
-    alignItems: "center",
-    justifyContent : "center",
+    alignContent: "center",
+    borderRadius: 15,
     margin: 10,
-    width : Platform.OS ==="android" ? "40%" : "45%", // android 40  / ios 45 
-    height : Platform.OS === "android" ? "23%" : "24%" // android 23 / ios 38
+    width: Platform.OS === "android" ? "42%" : "43%", // android 40  / ios 45
+    height: Platform.OS === "android" ? "22%" : "23%", // android 23 / ios 38
   },
   text: {
     textAlign: "center",
-    justifyContent : "flex-end",
-    fontWeight : "bold",
-    fontSize : 17
+    justifyContent: "center",
+    fontWeight: "bold",
+    fontSize: 17,
   },
   instructions: {
     textAlign: "center",
     color: "#333333",
     marginBottom: 10,
   },
- 
 });
 export default Categories;
