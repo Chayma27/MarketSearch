1a6c6b27048fce5d4519d2b28d32cfef4b14feb5
Guesmi Aslen <aslen9@hotmail.fr>
2022-02-22 17:18:05 +0100

    sous categories vehicules finished

--- src/screens/S_catg/Scvehicule.js
+++ src/screens/S_catg/Scvehicule.js
@@ -1,26 +1,73 @@
 import React from "react";
 import {View,Text,StyleSheet} from  'react-native'
-
+import Cardsctg from "../../components/sousCtg/Cardsctg";
+import { Avatar, Card, Title, Paragraph, Badge } from "react-native-paper";
+import { AntDesign } from "@expo/vector-icons";
 
 const Scvehicule = (props) => { 
-    return(
-        <View style = {styles.container}>
-            <Text style = {styles.titleHeader}> Sous categories ! Véhicule ! </Text>
+    return (
+        <View style={styles.parent}>
+          <View style={styles.container}>
+            <Card style={styles.cardStyle}>
+              <Cardsctg
+                count="302"
+                path={require("../../../assets/iconCategories/voiture.png")}
+                title="Voiture"
+              />
+            </Card>
+            <Card style={styles.cardStyle}>
+              <Cardsctg
+                count="27"
+                path={require("../../../assets/iconCategories/moto.png")}
+                title="Moto"
+              />
+            </Card>
+            <Card style={styles.cardStyle}>
+              <Cardsctg
+                count="0"
+                path={require("../../../assets/iconCategories/camion.png")}
+                title="Camion"
+              />
+            </Card>
+            
+            <Card style={styles.cardStyle}>
+          <Cardsctg
+            count="165"
+            path={require("../../../assets/iconCategories/otherr.png")}
+            title="Autres"
+          />
+        </Card>
+          </View>
         </View>
-    )
-}
+    
+      );
+    };
+    
+    const styles = StyleSheet.create({
+      parent: {
+        backgroundColor: "white",
+        flex: 1,
+      },
+    
+      container: {
+        marginTop: "10%",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      },
+      cardStyle: {
+        marginBottom: "3%",
+        backgroundColor: "#F0F0F0",
+        borderRadius: 20,
+        width: "96%",
+      },
+      titleHeader: {
+        fontSize: 17,
+        textAlign: "center",
+        fontWeight: "bold",
+      },
+    });
 
-
-const styles = StyleSheet.create({
-    container : { 
-        flex: 1 
-    }, 
-    titleHeader: { 
-        fontSize : 17, 
-        textAlign : 'center', 
-        fontWeight : 'bold'
-    }
-})
 
 
 export default Scvehicule
