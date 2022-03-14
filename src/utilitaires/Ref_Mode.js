e26a66423a17bb319a88892ef0a9dea4134730ce
Chayma <arfaouichayma27@gmail.com>
2022-03-14 11:57:16 +0100

    ajouter VetH et VetEnf

--- src/utilitaires/Ref_Mode.js
+++ src/utilitaires/Ref_Mode.js
@@ -38,7 +38,7 @@ export const VetF = [
           value: "Autres",
         },
       ],
-      TailleF: [
+      Taille: [
         {
           value: "XXS",
         },
@@ -221,6 +221,266 @@ export const VetF = [
         },
         {
           value: "Etat satisfaisant",
+        },
+      ],
+    },
+  ];
+  
+  export const VetH = [
+    {
+      TypeVetement: [
+        {
+          value: "Monteaux & Vestes",
+        },
+        {
+          value: "T-shirts",
+        },
+        {
+          value: "Pulls",
+        },
+        {
+          value: "Polos",
+        },
+        {
+          value: "Jillets",
+        },
+        {
+          value: "Capuches",
+        },
+        {
+          value: "Chemise",
+        },
+        {
+          value: "Costume ",
+        },
+        {
+          value: "Jeans",
+        },
+        {
+          value: "Short",
+        },
+        {
+          value: "Autres",
+        },
+      ],
+      Taille: [
+        {
+          value: "XS",
+        },
+        {
+          value: "S",
+        },
+        {
+          value: "M",
+        },
+        {
+          value: "L",
+        },
+        {
+          value: "XL",
+        },
+        {
+          value: "XXL",
+        },
+        {
+          value: "XXXL et plus",
+        },
+      ],
+    },
+  ];
+
+  export const VetEnf = [
+    {
+      TypeVetement: [
+        {
+          value: "Body",
+        },
+        {
+          value: "T-shirts/Sous pull",
+        },
+        {
+          value: "Polos",
+        },
+        {
+          value: "Chemise",
+        },
+        {
+          value: "Robes",
+        },
+        {
+          value: "Jupes",
+        },
+        {
+          value: "Jeans",
+        },
+        {
+          value: "Pantalon",
+        },
+        {
+          value: "Short",
+        },
+        {
+          value: "Salopette",
+        },
+        {
+          value: "Monteaux & Vestes",
+        },
+        {
+          value: "Chaussettes ",
+        },
+        {
+          value: "Cape/Paignoir ",
+        },
+        {
+          value: "Autres",
+        },
+      ],
+      Taille: [
+        {
+          value: "1mois - 12mois",
+        },
+        {
+          value: "1an",
+        },
+        {
+          value: "2ans",
+        },
+        {
+          value: "3ans",
+        },
+        {
+          value: "4ans",
+        },
+        {
+          value: "5ans",
+        },
+        {
+          value: "6ans",
+        },
+        {
+          value: "7ans",
+        },
+        {
+          value: "8ans",
+        },
+        {
+          value: "9ans",
+        },
+        {
+          value: "10ans",
+        },
+        {
+          value: "11ans",
+        },
+        {
+          value: "12ans",
+        },
+        {
+          value: "13ans",
+        },
+        {
+          value: "14ans",
+        },
+      ],
+      Marque: [
+        {
+          value: "Zara",
+        },
+        {
+          value: "Pimkie",
+        },
+        {
+          value: "Primark",
+        },
+        {
+          value: "Bershka",
+        },
+        {
+          value: "Adidas",
+        },
+        {
+          value: "Pull & Bear",
+        },
+        {
+          value: "Promod",
+        },
+        {
+          value: "Camaïeu",
+        },
+        {
+          value: "Baby Dior",
+        },
+        {
+          value: "Etam",
+        },
+        {
+          value: "H&M",
+        },
+        {
+          value: "Guess",
+        },
+        {
+          value: "Jules",
+        },
+        {
+          value: "C&A",
+        },
+        {
+          value: "Stardivarius",
+        },
+        {
+          value: "Dolce & Gabana",
+        },
+        {
+          value: "Lacoste",
+        },
+        {
+          value: "Levis",
+        },
+        {
+          value: "Louis Vuitton",
+        },
+        {
+          value: "Chanel",
+        },
+        {
+          value: "Nike",
+        },
+        {
+          value: "Yves Saint Lorent",
+        },
+        {
+          value: "Kaporal",
+        },
+        {
+          value: "Lee Cooper",
+        },
+        {
+          value: "Shein",
+        },
+        {
+          value: "Vertbaudet",
+        },
+        {
+          value: "Converse",
+        },
+        {
+          value: "Tape à L'oeil",
+        },
+        {
+          value: "  Hugo Boss",
+        },
+        {
+          value: "Petit Bateau",
+        },
+        {
+          value: "Pom Dapi",
+        },
+        {
+          value: "Sergent Major",
+        },
+        {
+          value: "Autres",
         },
       ],
     },
