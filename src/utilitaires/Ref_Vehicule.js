61398792d8a6c4f2c93ce42300149565e181dd8d
Chayma <arfaouichayma27@gmail.com>
2022-03-15 00:18:44 +0100

    Terminer fichier Ref_Vehicule

---
+++ src/utilitaires/Ref_Vehicule.js
@@ -0,0 +1,797 @@
+export const M_Audi = [
+    {
+      value: "100",
+    },
+    {
+        value: "200",
+      },
+      {
+        value: "80",
+      },
+      {
+        value: "90",
+      },
+      {
+        value: "A1",
+      },
+      {
+        value: "A2",
+      },
+      {
+        value: "A3",
+      },
+      {
+        value: "A4",
+      },
+      {
+        value: "A5",
+      },
+      {
+        value: "A6",
+      },
+      {
+        value: "A7",
+      },
+      {
+        value: "A8",
+      },
+      {
+        value: "Coup",
+      },
+      {
+        value: "Q1",
+      },
+      {
+        value: "Q2",
+      },
+      {
+        value: "Q3",
+      },
+      {
+        value: "Q7",
+      },
+      {
+        value: "Q8",
+      },
+      {
+        value: "Autres",
+      },
+  ];
+  
+
+  export const M_BMW = [
+    {
+      value: "316",
+    },
+    {
+        value: "318",
+      },
+      {
+        value: "320",
+      },
+      {
+        value: "323",
+      },
+      {
+        value: "324",
+      },
+      {
+        value: "325",
+      },
+      {
+        value: "518",
+      },
+      {
+        value: "520",
+      },
+      {
+        value: "504",
+      },
+      {
+        value: "Classe 5",
+      },
+      {
+        value: "I3",
+      },
+      {
+        value: "I8",
+      },
+      {
+        value: "M2",
+      },
+      {
+        value: "M3",
+      },
+      {
+        value: "M4",
+      },
+      {
+        value: "M5",
+      },
+      {
+        value: "M6",
+      },
+      {
+        value: "Serie 1",
+      },
+      {
+        value: "Serie 2",
+      },
+      {
+        value: "Serie 3",
+      },
+      {
+        value: "Serie 4",
+      },
+      {
+        value: "Serie 5",
+      },
+      {
+        value: "Serie 6",
+      },
+      {
+        value: "Serie 7",
+      },
+      {
+        value: "Serie 8",
+      },
+      {
+        value: "X1",
+      },
+      {
+        value: "X2",
+      },
+      {
+        value: "X3",
+      },
+      {
+        value: "X4",
+      },
+      {
+        value: "X5",
+      },
+      {
+        value: "X6",
+      },
+      {
+        value: "Autres",
+      },
+  ];
+  export const M_Mercedes = [
+    {
+      value: "190_series",
+    },
+    {
+        value: "200",
+      },
+      {
+        value: "220",
+      },
+      {
+        value: "230",
+      },
+      {
+        value: "240",
+      },
+      {
+        value: "250",
+      },
+      {
+        value: "260",
+      },
+      {
+        value: "400",
+      },
+      {
+        value: "500",
+      },
+      {
+        value: "560",
+      },
+      {
+        value: "AM6",
+      },
+      {
+        value: "CLS",
+      },
+      {
+        value: "Classe A",
+      },
+      {
+        value: "Classe B",
+      },
+      {
+        value: "Classe C",
+      },
+      {
+        value: "Classe E",
+      },
+      {
+        value: "Classe G",
+      },
+      {
+        value: "Classe GLE",
+      },
+      {
+        value: "Classe GLA",
+      },
+      {
+        value: "Classe M",
+      },
+      {
+        value: "Classe GLC",
+      },
+      {
+        value: "Classe GLS",
+      },
+      {
+        value: "MB",
+      },
+      {
+        value: "Autres",
+      },
+  ];
+
+  export const M_Citroen = [
+    {
+      value: "2Cv",
+    },
+    {
+        value: "Ax",
+      },
+      {
+        value: "Axel",
+      },
+      {
+        value: "Berlingo",
+      },
+      {
+        value: "Bx",
+      },
+      {
+        value: "C-crosser",
+      },
+      {
+        value: "C1",
+      },
+      {
+        value: "C15",
+      },
+      {
+        value: "C2",
+      },
+      {
+        value: "C25D",
+      },
+      {
+        value: "C3",
+      },
+      {
+        value: "C5",
+      },
+      {
+        value: "C6",
+      },
+      {
+        value: "C8",
+      },
+      {
+        value: "Cx",
+      },
+      {
+        value: "DS 3",
+      },
+      {
+        value: "DS 4",
+      },
+      {
+        value: "DS 5",
+      },
+      {
+        value: "Dyane",
+      },
+      {
+        value: "Xm",
+      },
+      {
+        value: "Zx",
+      },
+      {
+        value: "Autres",
+      },
+  ];
+
+
+  export const M_Yamaha = [
+    {
+      value: "Aerox",
+    },
+    {
+        value: "AG",
+      },
+      {
+        value: "BT",
+      },
+      {
+        value: "BW's",
+      },
+      {
+        value: "DT",
+      },
+      {
+        value: "DTR",
+      },
+      {
+        value: "EC",
+      },
+      {
+        value: "FJ",
+      },
+      {
+        value: "FZ",
+      },
+      {
+        value: "GT",
+      },
+      {
+        value: "MT",
+      },
+      {
+        value: "XJ",
+      },
+      {
+        value: "Autres",
+      },
+  ];
+
+  export const M_Honda = [
+    {
+      value: "AFS",
+    },
+    {
+        value: "CA",
+      },
+      {
+        value: "CB",
+      },
+      {
+        value: "CM",
+      },
+      {
+        value: "CR",
+      },
+      {
+        value: "CRF",
+      },
+      {
+        value: "CRM",
+      },
+      {
+        value: "GL",
+      },
+      {
+        value: "Lead",
+      },
+      {
+        value: "NC",
+      },
+      {
+        value: "N7x",
+      },
+      {
+        value: "Autres",
+      },
+  ];
+
+  export const Voiture = [
+    {
+      Marque: [
+        {
+          value: "Audi",
+        },
+        {
+          value: "BMW",
+        },
+        {
+          value: "Mercedes",
+        },
+        {
+          value: "Citroen",
+        },
+        {
+          value: "Fiat",
+        },
+        {
+            value: "Opet",
+          },
+          {
+            value: "Peugeot",
+          },
+          {
+            value: "Renault",
+          },
+          {
+            value: "Volswagen",
+          },
+          {
+            value: "DS",
+          },
+          {
+            value: "Hummer",
+          },
+          {
+            value: "Hyndai",
+          },
+          {
+            value: "Isuzu",
+          },
+          {
+            value: "Jaguar",
+          },
+          {
+            value: "Jeep",
+          },
+          {
+            value: "Kia",
+          },
+          {
+            value: "Lamborgini",
+          },
+          {
+            value: "Rang Rover",
+          },
+          {
+            value: "Mazda",
+          },
+          {
+            value: "Mini",
+          },
+          {
+            value: "Nissan",
+          },
+          {
+            value: "Porsche",
+          },
+          {
+            value: "Tesla",
+          },
+        {
+          value: "Autres",
+        },
+      ],
+      AnneeModele: [
+        {
+          value: "2022",
+        },
+        {
+            value: "2021",
+        },
+        {
+            value: "2020",
+        },
+        {
+            value: "2019",
+        },
+        {
+            value: "2018",
+        },
+        {
+            value: "2017",
+        },
+        {
+            value: "2016",
+        },
+        {
+            value: "2015",
+        },
+        {
+            value: "2014",
+        },
+        {
+            value: "2013",
+        },
+        {
+            value: "2012",
+        },
+        {
+            value: "2011",
+        },
+        {
+            value: "2010",
+        },
+        {
+            value: "2009",
+        },
+        {
+            value: "2008",
+        },
+        {
+            value: "2007",
+        },
+        {
+            value: "2006",
+        },
+        {
+            value: "2005",
+        },
+        {
+            value: "2004",
+        },
+        {
+            value: "2003",
+        },
+        {
+            value: "2002",
+        },
+        {
+            value: "2001",
+        },
+        {
+            value: "2000",
+        },
+        {
+            value: "1999",
+        },
+        {
+            value: "1998",
+        },
+        {
+            value: "1997",
+        },
+        {
+            value: "1996",
+        },
+        {
+            value: "1995",
+        },
+        {
+            value: "1994",
+        },
+        {
+            value: "1993",
+        },
+        {
+            value: "1992",
+        },
+        {
+            value: "1991",
+        },
+        {
+            value: "1990",
+        },
+        {
+            value: "1989",
+        },
+        {
+            value: "1988",
+        },
+        {
+            value: "1987",
+        },
+        {
+            value: "1986",
+        },
+        {
+            value: "1985",
+        },
+        {
+            value: "1984",
+        },
+        {
+            value: "1983",
+        },
+        {
+            value: "1982",
+        },
+        {
+            value: "1981",
+        },
+        {
+            value: "1980 ou avant",
+        },
+      ],
+      Carburant: [
+        {
+          value: "Essence",
+        },
+        {
+          value: "Diesel",
+        },
+        {
+          value: "Hybride",
+        },
+        {
+          value: "Electrique",
+        },
+        {
+          value: "GPL",
+        },
+        {
+            value: "Autres",
+        },
+    ],
+    BoiteVitesse: [
+        {
+          value: "Manuelle",
+        },
+        {
+          value: "Automatique",
+        },
+    ],
+    NbrPorte: [
+        {
+          value: "2",
+        },
+        {
+            value: "3",
+        },
+        {
+            value: "4",
+        },
+        {
+            value: "5",
+        },
+        {
+            value: "6 ou plus",
+        },
+    ],
+    NbrPlace: [
+        {
+            value: "1",
+        },
+        {
+          value: "2",
+        },
+        {
+            value: "3",
+        },
+        {
+            value: "4",
+        },
+        {
+            value: "5",
+        },
+        {
+            value: "6",
+        },
+        {
+            value: "7 ou plus",
+        },
+    ],
+    Couleur: [
+        {
+          value: "Blanc",
+        },
+        {
+          value: "Noir",
+        },
+        {
+          value: "Beije/Camel",
+        },
+        {
+          value: "Rouge/Bordeaux",
+        },
+        {
+          value: "Rose",
+        },
+        {
+          value: "Vert",
+        },
+        {
+          value: "Violet",
+        },
+        {
+          value: "Orange",
+        },
+        {
+          value: "Orange",
+        },
+        {
+          value: "Multicolor",
+        },
+        {
+          value: "Marron",
+        },
+        {
+          value: "Jaune/Moutarde",
+        },
+        {
+          value: "Imprimé",
+        },
+        {
+          value: "Gris",
+        },
+        {
+          value: "Doré",
+        },
+        {
+          value: "Bleu/Ciel",
+        },
+        {
+          value: "Argenté",
+        },
+        {
+          value: "Autres",
+        },
+      ],
+      TypeVehicule: [
+        {
+          value: "4x4 / SUV",
+        },
+        {
+          value: "Berline",
+        },
+        {
+          value: "Cabriolet",
+        },
+        {
+          value: "Coupé",
+        },
+    ],
+
+},
+];
+
+export const Moto = [ //AnneModele, Couleur
+    {
+      Marque: [
+        {
+          value: "Yamaha",
+        },
+        {
+          value: "Honda",
+        },
+        {
+          value: "Suzuki",
+        },
+        {
+          value: "Hawasaki",
+        },
+        {
+          value: "BBW",
+        },
+        {
+            value: "Benelli",
+        },
+        {
+          value: "Autres",
+        },
+      ],
+
+      Type: [
+        {
+          value: "Moto",
+        },
+        {
+          value: "Scooter",
+        },
+        {
+          value: "Quad",
+        },
+      ],
+
+},
+];
+
+export const Camion = [ //AnneModele, Carburant, BoiteVitesse, Couleur, NbrPorte, NbrPlace
+    {
+      Marque: [
+        {
+          value: "Renault",
+        },
+        {
+          value: "Peugeot",
+        },
+        {
+          value: "Citroen",
+        },
+        {
+          value: "Fiat",
+        },
+        {
+          value: "Autres",
+        },
+      ],
+
+},
+];
