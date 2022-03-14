af0edb0565430ed98c8adf224627b317ec9a3f2e
Chayma <arfaouichayma27@gmail.com>
2022-03-14 13:00:17 +0100

    ajouter tt le reste des sous categ

--- src/utilitaires/Ref_Mode.js
+++ src/utilitaires/Ref_Mode.js
@@ -485,3 +485,711 @@ export const VetF = [
       ],
     },
   ];
+
+  export const Chauss = [   
+    {
+      Univers: [
+        {
+          value: "Femme",
+        },
+        {
+          value: "Homme",
+        },
+        {
+          value: "Enfant",
+        },
+      ],
+      TypeChaussure: [
+        {
+          value: "Baskets",
+        },
+        {
+          value: "Moccasins",
+        },
+        {
+          value: "Chaussures à scratch",
+        },
+        {
+          value: "Bottines",
+        },
+        {
+          value: "Bottes",
+        },
+        {
+          value: "Escarpins",
+        },
+        {
+          value: "Sandales",
+        },
+        {
+          value: "Pantoufles",
+        },
+        {
+          value: "Ballerines",
+        },
+        {
+          value: "Autres",
+        },
+      ],
+      Pointure: [
+        {
+          value: "16",
+        },
+        {
+          value: "17",
+        },
+        {
+          value: "18",
+        },
+        {
+          value: "19",
+        },
+        {
+          value: "20",
+        },
+        {
+          value: "21",
+        },
+        {
+          value: "22",
+        },
+        {
+          value: "23",
+        },
+        {
+          value: "24",
+        },
+        {
+          value: "25",
+        },
+        {
+          value: "26",
+        },
+        {
+          value: "27",
+        },
+        {
+          value: "28",
+        },
+        {
+          value: "29",
+        },
+        {
+          value: "30",
+        },
+        {
+          value: "31",
+        },
+        {
+          value: "32",
+        },
+        {
+          value: "33",
+        },
+        {
+          value: "34",
+        },
+        {
+          value: "35",
+        },
+        {
+          value: "36",
+        },
+        {
+          value: "37",
+        },
+        {
+          value: "38",
+        },
+        {
+          value: "39",
+        },
+        {
+          value: "40",
+        },
+        {
+          value: "41",
+        },
+        {
+          value: "42",
+        },
+        {
+          value: "43",
+        },
+        {
+          value: "44",
+        },
+        {
+          value: "45",
+        },
+        {
+          value: "46",
+        },
+        {
+          value: "47",
+        },
+        {
+          value: "48",
+        },
+        {
+          value: "49",
+        },
+        {
+          value: "50 et plus",
+        },
+      ],
+      Marque: [
+        {
+          value: "Zara",
+        },
+        {
+          value: "Pimkie",
+        },
+        {
+          value: "Primark",
+        },
+        {
+          value: "Bershka",
+        },
+        {
+          value: "Adidas",
+        },
+        {
+          value: "Pull & Bear",
+        },
+        {
+          value: "Promod",
+        },
+        {
+          value: "Camaïeu",
+        },
+        {
+          value: "Dior",
+        },
+        {
+          value: "H&M",
+        },
+        {
+          value: "Guess",
+        },
+        {
+          value: "Jules",
+        },
+        {
+          value: "C&A",
+        },
+        {
+          value: "Stardivarius",
+        },
+        {
+          value: "Dolce & Gabana",
+        },
+        {
+          value: "Lacoste",
+        },
+        {
+          value: "Levis",
+        },
+        {
+          value: "Louis Vuitton",
+        },
+        {
+          value: "Chanel",
+        },
+        {
+          value: "Nike",
+        },
+        {
+          value: "Yves Saint Lorent",
+        },
+        {
+          value: "Kaporal",
+        },
+        {
+          value: "Lee Cooper",
+        },
+        {
+          value: "Shein",
+        },
+        {
+          value: "Converse",
+        },
+        {
+          value: "Aigle",
+        },
+        {
+          value: "Asics",
+        },
+        {
+          value: "Barker",
+        },
+        {
+          value: "Azzaro",
+        },
+        {
+          value: "Asos",
+        },
+        {
+          value: "DC Shoes",
+        },
+        {
+          value: "Coq Sportif",
+        },
+        {
+          value: "Jimmy Choo",
+        },
+        {
+          value: "New Balance",
+        },
+        {
+          value: "Alexander McQueen",
+        },
+        {
+          value: "Autres",
+        },
+      ],
+    },
+  ];
+
+  export const SacBag = [
+    {
+      Type: [
+        {
+          value: "Sac à main",
+        },
+        {
+          value: "Cartable",
+        },
+        {
+          value: "Valise",
+        },
+        {
+          value: "Trousse",
+        },
+        {
+          value: "Sacoche",
+        },
+        {
+          value: "Sac à bandoulière",
+        },
+        {
+          value: "Sac à dos",
+        },
+        {
+          value: "Porte monnais et porte feuille",
+        },
+        {
+          value: "Pochette",
+        },
+        {
+          value: "Accessoire cheveux",
+        },
+        {
+          value: "Bonnet",
+        },
+        {
+          value: "Casquette",
+        },
+        {
+          value: "Cravate et noeud papillon",
+        },
+        {
+          value: "Gants",
+        },
+        {
+          value: "Etui Tel",
+        },
+        {
+          value: "Porte Clé",
+        },
+        {
+          value: "Housse de tablette et orfinateur",
+        },
+        {
+          value: "Autres",
+        },
+      ],
+      Marque: [
+        {
+          value: "Zara",
+        },
+        {
+          value: "Pimkie",
+        },
+        {
+          value: "Primark",
+        },
+        {
+          value: "Bershka",
+        },
+        {
+          value: "Adidas",
+        },
+        {
+          value: "Pull & Bear",
+        },
+        {
+          value: "Promod",
+        },
+        {
+          value: "Camaïeu",
+        },
+        {
+          value: "Dior",
+        },
+        {
+          value: "Etam",
+        },
+        {
+          value: "H&M",
+        },
+        {
+          value: "Guess",
+        },
+        {
+          value: "Jules",
+        },
+        {
+          value: "C&A",
+        },
+        {
+          value: "Stardivarius",
+        },
+        {
+          value: "Dolce & Gabana",
+        },
+        {
+          value: "Lacoste",
+        },
+        {
+          value: "Levis",
+        },
+        {
+          value: "Louis Vuitton",
+        },
+        {
+          value: "Chanel",
+        },
+        {
+          value: "Nike",
+        },
+        {
+          value: "Yves Saint Lorent",
+        },
+        {
+          value: "Kaporal",
+        },
+        {
+          value: "Lee Cooper",
+        },
+        {
+          value: "Shein",
+        },
+        {
+          value: "Alexander McQueen",
+        },
+        {
+          value: "valentino",
+        },
+        {
+          value: "Michael Kors",
+        },
+        {
+          value: "Coach",
+        },
+        {
+          value: "karl",
+        },
+        {
+          value: "Hermes",
+        },
+        {
+          value: "Autres",
+        }, 
+      ],
+      Matiere: [
+        {
+          value: "Chaine",
+        },
+        {
+          value: "Coton",
+        },
+        {
+          value: "Cuir",
+        },
+        {
+          value: "Cuir vernis",
+        },
+        {
+          value: "Dain",
+        },
+        {
+          value: "Laine",
+        },
+        {
+          value: "Nylon",
+        },
+        {
+          value: "Plastique",
+        },
+        {
+          value: "Python",
+        },
+        {
+          value: "Satain",
+        },
+        {
+          value: "Simili cuir",
+        },
+        {
+          value: "Soie",
+        },
+        {
+          value: "Tissu",
+        },
+        {
+          value: "Synthétique",
+        },
+        {
+          value: "Velours",
+        },
+        {
+          value: "Autres",
+        },
+      ],
+    },
+  ];
+
+  export const BijMontre = [
+    {
+      {
+        Univers: [
+          {
+            value: "Femme",
+          },
+          {
+            value: "Homme",
+          },
+          {
+            value: "Enfant",
+          },
+          {
+            value: "Mixte",
+          },
+        ],
+        Type: [
+          {
+            value: "Bague",
+          },
+          {
+            value: "Boucles d'oreilles",
+          },
+          {
+            value: "Bracelet",
+          },
+          {
+            value: "Broche",
+          },
+          {
+            value: "Chaine",
+          },
+          {
+            value: "Collier/Pendatif",
+          },
+          {
+            value: "Montre à gouset",
+          },
+          {
+            value: "Montre auto",
+          },
+          {
+            value: "Montre connecté",
+          },
+          {
+            value: "Montre quartz",
+          },
+          {
+            value: "Parure",
+          },
+          {
+            value: "Piercing",
+          },
+          {
+            value: "Bijoux de tête",
+          },
+          {
+            value: "Autres",
+          },
+        ],
+        Marque: [
+          {
+            value: "APM",
+          },
+          {
+            value: "Apple",
+          },
+          {
+            value: "Big bang bijoux",
+          },
+          {
+            value: "Bijoux victoria",
+          },
+          {
+            value: "Barberry",
+          },
+          {
+            value: "Cartier",
+          },
+          {
+            value: "Céline",
+          },
+          {
+            value: "chaumet",
+          },
+          {
+            value: "Chopard",
+          },
+          {
+            value: "Danile Wellington",
+          },
+          {
+            value: "Fendi",
+          },
+          {
+            value: "Festina",
+          },
+          {
+            value: "Fossil",
+          },
+          {
+            value: "Gas bijoux",
+          },
+          {
+            value: "Histoire d'or",
+          },
+          {
+            value: "Hugo Boss",
+          },
+          {
+            value: "Ice Watch",
+          },
+          {
+            value: "Messika",
+          },
+          {
+            value: "Michael Kors",
+          },
+          {
+            value: "Pandora",
+          },
+          {
+            value: "Parfois",
+          },
+          {
+            value: "Rolex",
+          },
+          {
+            value: "Samsung",
+          },
+          {
+            value: "Swatch",
+          },
+          {
+            value: "Swarovski",
+          },
+          {
+            value: "Tiffany & Co",
+          },
+ 
+          {
+            value: "Bershka",
+          },
+          {
+            value: "Dior",
+          },
+          {
+            value: "Guess",
+          },
+          {
+            value: "Dolce & Gabana",
+          },
+          {
+            value: "Louis Vuitton",
+          },
+          {
+            value: "Chanel",
+          },
+          {
+            value: "Yves Saint Lorent",
+          },
+          {
+            value: "Shein",
+          },
+          {
+            value: "Hermes",
+          },
+          {
+            value: "Diamond Factory",
+          },
+          {
+            value: "Celinni",
+          },
+          {
+            value: "Autres",
+          }, 
+        ],
+        Matiere: [
+          {
+            value: "Acier",
+          },
+          {
+            value: "Acier Inoxydable",
+          },
+          {
+            value: "Argent",
+          },
+          {
+            value: "Or Blanc",
+          },
+          {
+            value: "Or Jaune",
+          },
+          {
+            value: "Or Rose",
+          },
+          {
+            value: "Bois",
+          },
+          {
+            value: "Céramique",
+          },
+          {
+            value: "Cuir",
+          },
+          {
+            value: "Diamond",
+          },
+          {
+            value: "Métal",
+          },
+          {
+            value: "Perle",
+          },
+          {
+            value: "Pierre",
+          },
+          {
+            value: "Or Plaqué",
+          },
+          {
+            value: "Tissu",
+          },
+          {
+            value: "Plastique",
+          },
+          {
+            value: "Autres",
+          },
+        ],
+    },
+  ];
