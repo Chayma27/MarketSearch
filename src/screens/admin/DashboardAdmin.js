f23d21466bed8b4e011abe416517a51b840f1894
Guesmi Aslen <aslen9@hotmail.fr>
2022-04-20 16:03:06 +0200

    add icon

--- src/screens/admin/DashboardAdmin.js
+++ src/screens/admin/DashboardAdmin.js
@@ -6,8 +6,10 @@ import {
   Image,
   TextInput,
   ImageBackground,
+  TouchableOpacity,
 } from "react-native";
 import { Button } from "react-native-elements";
+import Carousel from "simple-carousel-react-native";
 
 const DashboardAdmin = (props) => {
   const image = require("../../../assets/background-image-admin.jpeg");
@@ -15,6 +17,66 @@ const DashboardAdmin = (props) => {
   return (
     <View style={styles.container}>
       <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
+          }}
+        >
+          <Carousel backgroundColor="transparent" showScroll={ false } showBubbles={true}>
+            <TouchableOpacity>
+              <Image
+                style={{ width: 300, height: 300, marginLeft: "10%" }}
+                source={require("../../../assets/banned-user.png")}
+              />
+              <Text
+                style={{
+                  fontSize: 18,
+                  fontWeight: "bold",
+                  textAlign: "center",
+                  marginTop: "5%",
+                }}
+              >
+                Gerer utilisateur
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+              <Image
+                style={{ width: 300, height: 300, marginLeft: "10%" }}
+                source={require("../../../assets/panier.png")}
+              />
+              <Text
+                style={{
+                  fontSize: 18,
+                  fontWeight: "bold",
+                  textAlign: "center",
+                  marginTop: "5%",
+                }}
+              >
+                Gerer Annonces
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+              <Image
+                style={{ width: 300, height: 300, marginLeft: "10%" }}
+                source={require("../../../assets/logout.png")}
+              />
+              <Text
+                style={{
+                  fontSize: 18,
+                  fontWeight: "bold",
+                  textAlign: "center",
+                  marginTop: "5%",
+                }}
+              >
+                Se deconnecter
+              </Text>
+            </TouchableOpacity>
+          </Carousel>
+        </View>
       </ImageBackground>
     </View>
   );
@@ -26,8 +88,8 @@ const styles = StyleSheet.create({
   },
   image: {
     flex: 1,
+    justifyContent: "center",
   },
-
 });
 
 export default DashboardAdmin;
