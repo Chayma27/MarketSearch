3da1a4bb6e4c3c5774c16554ca9b72fc7f950378
Guesmi Aslen <aslen9@hotmail.fr>
2022-06-06 16:00:04 +0200

    flatlist display card

--- src/screens/MesArticles.js
+++ src/screens/MesArticles.js
@@ -1,80 +1,77 @@
 import React from "react";
-import {View,Text,StyleSheet} from  'react-native'
+import { View, Text, StyleSheet, FlatList } from "react-native";
 import Cardsctg from "../components/sousCtg/Cardsctg";
 import { Avatar, Card, Title, Paragraph, Badge } from "react-native-paper";
 
-const MesArticles = (props) => { 
-    return(
-        <View style={styles.parent}>
-        <View style={styles.container}>
-            <Card style={styles.cardStyle}>
-                <Cardsctg
-                    count="285"
-                    path={require("../../assets/iconCategories/Jeux.png")}
-                    title="Jeux et Jouets"
-                />
-            </Card>
-            <Card style={styles.cardStyle}>
-                <Cardsctg
-                    count="12"
-                    path={require("../../assets/iconCategories/Musique.png")}
-                    title="Instruments de musqiue"
-                />
-            </Card>
-            <Card style={styles.cardStyle}>
-                <Cardsctg
-                    count="62"
-                    path={require("../../assets/iconCategories/Livre.png")}
-                    title="Livres"
-                />
-            </Card>
-            <Card style={styles.cardStyle}>
-                <Cardsctg
-                    count="29"
-                    path={require("../../assets/iconCategories/CD.png")}
-                    title="CD/DVD"
-                />
-            </Card>
-            
-            <Card style={styles.cardStyle}>
-                <Cardsctg
-                    count="0"
-                    path={require("../../assets/iconCategories/otherr.png")}
-                    title="Autres"
-                />
-            </Card>
+const MesArticles = (props) => {
+  const data = [
+    {
+      id: 1,
+      count: 285,
+      path:require("../../assets/iconCategories/Jeux.png"),
+      title: "Jeux et Jouets",
+    },
+    {
+      id: 2,
+      count: 12,
+      path:require("../../assets/iconCategories/Livre.png"),
+      title: "Livres",
+    },
+    {
+      id: 3,
+      count: 285,
+      path:require("../../assets/iconCategories/Jeux.png"),
+      title: "Jeux et Jouets",
+    },
+    
+  ];
+  const renderItem = ({ item }) => (
+    <View style={styles.container}>
+    <Card style={styles.cardStyle}>
+      <Cardsctg
+        count={item.count}
+        path={item.path}
+        title={item.title}
+      />
+    </Card>
+      </View>
+  );
+  return (
+    <View style={styles.parent}>
 
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+        />
+          </View>
 
-        </View>
-    </View>
-    )
-}
-
+  );
+};
 
 const styles = StyleSheet.create({
-    parent: {
-        backgroundColor: "white",
-        flex: 1,
-    },
+  parent: {
+    backgroundColor: "white",
+    flex: 1,
+  },
 
-    container: {
-        marginTop: "10%",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    cardStyle: {
-        marginBottom: "3%",
-        backgroundColor: "#F0F0F0",
-        borderRadius: 20,
-        width: "96%",
-    },
-    titleHeader: {
-        fontSize: 17,
-        textAlign: "center",
-        fontWeight: "bold",
-    },
-})
+  container: {
+    marginTop: "10%",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cardStyle: {
+    marginBottom: "3%",
+    backgroundColor: "#F0F0F0",
+    borderRadius: 20,
+    width: "96%",
+  },
+  titleHeader: {
+    fontSize: 17,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+});
 
-
-export default MesArticles
+export default MesArticles;
