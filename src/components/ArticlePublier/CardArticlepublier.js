4d91c8f2b26be6de105f33885847de2779f29bc6
Guesmi Aslen <aslen9@hotmail.fr>
2022-06-08 17:02:22 +0200

    information icon add

--- src/components/ArticlePublier/CardArticlepublier.js
+++ src/components/ArticlePublier/CardArticlepublier.js
@@ -1,61 +1,67 @@
 import React from "react";
-import {Image, View, Text, StyleSheet , TouchableOpacity } from "react-native";
+import { Image, View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { Avatar, Card, Title, Paragraph, Badge } from "react-native-paper";
-import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialIcons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 const CardArticlepublier = (props) => {
-    const Message=() => { 
-        if (props.count > 1) { 
-            return "Plusieurs annonces sont disponibles"
-        } else if (props.count == 1){ 
-            return "Une seule annonce est disponible !"
-        } else { 
-            return "Pas d'annonces pour le moment ... "
-        }
+  const Message = () => {
+    if (props.count > 1) {
+      return "Plusieurs annonces sont disponibles";
+    } else if (props.count == 1) {
+      return "Une seule annonce est disponible !";
+    } else {
+      return "Pas d'annonces pour le moment ... ";
     }
+  };
   return (
-        <TouchableOpacity>
-            
-
-          <Card.Title
-            title={props.title}
-            subtitle={(
-                    <View>
-                     <Text>{props.sousCtg} </Text>
-                     <Text style={{fontWeight : '700'}}>{props.prix} € </Text>
-                        </View>
-                
-            )}
-            left={() => (
-              <Image
-                {...props}
-                style={{backgroundColor : "transparent"}}
-                source={props.path}
-                style={{width : 50 , height : 50}}
-              />
-            )}
-            right={() => (
-              <>
-              <TouchableOpacity>
-                <View
-                  style={{
-                    flexDirection: "row",
-                    justifyContent: "space-around",
-                  }}
-                >
-             
-                  <MaterialIcons name="delete" size={34} color="red"                     style={{ marginLeft: "10%" }}
- />
-                </View>
-                </TouchableOpacity>
-              </>
-            )}
+    <TouchableOpacity>
+      <Card.Title
+        title={props.title}
+        subtitle={
+          <View>
+            <Text>{props.sousCtg} </Text>
+            <Text style={{ fontWeight: "700" }}>{props.prix} € </Text>
+          </View>
+        }
+        left={() => (
+          <Image
+            {...props}
+            style={{ backgroundColor: "transparent" }}
+            source={props.path}
+            style={{ width: 50, height: 50 }}
           />
-            {/* <Card.Content>
+        )}
+        right={() => (
+          <>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-around",
+              }}
+            >
+              <TouchableOpacity>
+                <MaterialCommunityIcons
+                  name="information-outline"
+                  size={34}
+                  color="black"
+                />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <MaterialIcons
+                  name="delete"
+                  size={34}
+                  color="red"
+                  style={{ marginLeft: "10%" }}
+                />
+              </TouchableOpacity>
+            </View>
+          </>
+        )}
+      />
+      {/* <Card.Content>
               <Paragraph style={{ color: "grey", marginLeft: 55 }}>Prix : 125</Paragraph>
           </Card.Content> */}
-       
-          </TouchableOpacity>
-     
+    </TouchableOpacity>
   );
 };
 
@@ -67,15 +73,14 @@ const styles = StyleSheet.create({
 
   container: {
     marginTop: "10%",
-    flexDirection : 'column',
-    justifyContent : 'center',
-    alignItems : 'center',
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
   },
   cardStyle: {
-    
     backgroundColor: "#F0F0F0",
-    borderRadius : 20, 
-    width : "96%", 
+    borderRadius: 20,
+    width: "96%",
   },
   titleHeader: {
     fontSize: 17,
