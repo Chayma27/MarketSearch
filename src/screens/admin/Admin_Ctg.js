5d83007bbd92ac6b8322a33ac7efc483b49a4284
Guesmi Aslen <aslen9@hotmail.fr>
2022-06-30 10:30:49 +0200

    ajouter photo categorie admin

--- src/screens/admin/Admin_Ctg.js
+++ src/screens/admin/Admin_Ctg.js
@@ -1,9 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, View, ScrollView, TextInput, Image, Touchable, TouchableOpacity } from "react-native";
+import { StyleSheet, View, ScrollView, TextInput, Image, Touchable, TouchableOpacity, Platform } from "react-native";
 import { Text, Card, Icon, Input, Button } from "react-native-elements";
 import { Dropdown } from "react-native-material-dropdown-v2-fixed";
 import { PricingCard } from "react-native-elements";
+import * as ImagePicker from 'expo-image-picker';
 
 
 
@@ -17,9 +18,23 @@ import {
 const Admin_Ctg = (props) => {
   const [number, onChangeNumber] = React.useState(null);
   const [text, onChangeText] = React.useState("0");
-  const [selectedCtg, setSelectedCtg] = useState("");
-  const [selectedSCtg, setSelectedSCtg] = useState("");
+  const [image, setImage] = useState(null);
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
 
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+ 
   return (
     <ScrollView>
       <View style={styles.container}>
@@ -30,7 +45,7 @@ const Admin_Ctg = (props) => {
             {" "}
             Nom de la catégorie {" "}
           </Text>
-          <Input placeholder="Merci de saisir le nom " />
+          <Input placeholder="Merci de saisir le nom fsd" />
       
         </Card>
 
@@ -42,9 +57,12 @@ const Admin_Ctg = (props) => {
           >
             Ajouter une photo pour votre nouvelle catégorie
           </Text>
-          <TouchableOpacity style={{alignItems: 'center'}}>
+          <TouchableOpacity style={{alignItems: 'center'}} onPress={pickImage}>
           <Image style={styles.resizePicture} source={require('../../../assets/add-photo.png')} />
           </TouchableOpacity>
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+    </View>
         </Card>
         <Card>
        
@@ -61,6 +79,7 @@ const Admin_Ctg = (props) => {
             buttonStyle={{ backgroundColor: "#4f9deb" }}
           />
         </Card>
+      
       </View>
     </ScrollView>
   );
